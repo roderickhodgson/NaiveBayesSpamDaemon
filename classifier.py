b67331410f9f9c7c9ec80dfcddf779ca02ac55b8
Roderick Hodgson <roderick.hogson@googlemail.com>
2012-11-24 21:52:31 +0000

    Daemon classes to do RPC calls on classifier (to keep it in memory)

--- classifier.py
+++ classifier.py
@@ -1,10 +1,12 @@
 from os import listdir, path
+import settings
+import pika
+import uuid
 from nltk import NaiveBayesClassifier
 from nltk.tokenize import word_tokenize
 
-import settings
-
 class NaiveBayesDaemonClassifier(object):
+
     @staticmethod
     def data_files(dir):
         return [path.join(dir, f) for f in listdir(dir) if f[0]!="."]
@@ -47,4 +49,35 @@ class NaiveBayesDaemonClassifier(object):
         if self.classifier == None:
             self._init_classifier()
         return self.classifier.classify(self.get_word_features(message)) == 'spam'
+
+class NaiveBayesDaemonClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def is_message_spam(self, message):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='naive_bayes_rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=message)
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response == "True"
 
